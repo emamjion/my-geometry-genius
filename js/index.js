fb89c9ebbe9de0b3ab0d80dc16d5e7f158b3dd91
emamjion <emamjion@gmail.com>
2023-02-18 15:41:58 +0600

    added a getInputValueById function

---
+++ js/index.js
@@ -0,0 +1,36 @@
+/*
+    const shapeNameElement = document.getElementById('triangle');
+    const shapeName = shapeNameElement.innerText;
+*/
+
+/*
+    const base = baseElement.value;
+    const height = heightElement.value;
+*/
+
+/*
+    const shapeNameElement = document.getElementById('rectangle');
+    const shapeName = shapeNameElement.innerText;
+*/
+
+/*  const width = widthElement.value;
+    const length = lengthElement.value;
+*/
+
+/*
+    const shapeNameElement = document.getElementById('parallelogram');
+    const shapeName = shapeNameElement.innerText;
+*/
+
+/*
+    const shapeName = document.getElementById('rhombus').innerText;
+*/
+
+/*
+    const shapeName = document.getElementById('pentagon').innerText;
+*/
+
+/*
+    const shapeName = document.getElementById('ellipse').innerText;
+*/
+
--- js/script.js
+++ js/script.js
@@ -3,31 +3,26 @@ let serial = 0;
 // Triangle
 document.getElementById('triangle-btn').addEventListener('click',function(){
     serial = serial + 1;
+    
+    const shapeName = getInputValueById('triangle');
 
-    const shapeNameElement = document.getElementById('triangle');
-    const shapeName = shapeNameElement.innerText;
-
-    const baseElement = document.getElementById('triangle-base');
-    const heightElement = document.getElementById('triangle-height');
-    const base = baseElement.value;
-    const height = heightElement.value;
+    const base = document.getElementById('triangle-base').value;
+    const height = document.getElementById('triangle-height').value;
     
     const area = (0.5 * parseFloat(base) * parseFloat(height)).toFixed(2);
     calculation(serial,shapeName,area);
     
 });
 
+
 // rectangle
 document.getElementById('rectangle-btn').addEventListener('click',function(){
     serial = serial + 1;
+    
+    const shapeName = getInputValueById('rectangle');
 
-    const shapeNameElement = document.getElementById('rectangle');
-    const shapeName = shapeNameElement.innerText;
-
-    const widthElement = document.getElementById('rectangle-width');
-    const lengthElement = document.getElementById('rectangle-length');
-    const width = widthElement.value;
-    const length = lengthElement.value;
+    const width = document.getElementById('rectangle-width').value;
+    const length = document.getElementById('rectangle-length').value;
 
     const area = (parseFloat(width) * parseFloat(length)).toFixed(2);
     calculation(serial,shapeName,area);
@@ -36,8 +31,8 @@ document.getElementById('rectangle-btn').addEventListener('click',function(){
 // Parallelogram
 document.getElementById('parallelogram-btn').addEventListener('click',function(){
     serial = serial + 1;
-    const shapeNameElement = document.getElementById('parallelogram');
-    const shapeName = shapeNameElement.innerText;
+    
+    const shapeName = getInputValueById('parallelogram');
 
     const base = document.getElementById('parallelogram-base').value;
     const height = document.getElementById('parallelogram-height').value;
@@ -49,7 +44,8 @@ document.getElementById('parallelogram-btn').addEventListener('click',function()
 // Rhombus
 document.getElementById('rhombus-btn').addEventListener('click',function(){
     serial = serial + 1;
-    const shapeName = document.getElementById('rhombus').innerText;
+    
+    const shapeName = getInputValueById('rhombus');
 
     const d1 = document.getElementById('rhombus-d1').value;
     const d2 = document.getElementById('rhombus-d2').value;
@@ -61,8 +57,9 @@ document.getElementById('rhombus-btn').addEventListener('click',function(){
 // Pentagon
 document.getElementById('pentagon-btn').addEventListener('click',function(){
     serial = serial + 1;
-    const shapeName = document.getElementById('pentagon').innerText;
-
+    
+    const shapeName = getInputValueById('pentagon');
+    
     const p = document.getElementById('pentagon-p').value;
     const b = document.getElementById('pentagon-b').value;
 
@@ -73,23 +70,34 @@ document.getElementById('pentagon-btn').addEventListener('click',function(){
 // Ellipse
 document.getElementById('ellipse-btn').addEventListener('click',function(){
     serial = serial + 1;
-    const shapeName = document.getElementById('ellipse').innerText;
 
+    const shapeName = getInputValueById('ellipse');
+    
     const a = document.getElementById('ellipse-a').value;
     const b = document.getElementById('ellipse-b').value;
+    
     // pi value is = 3.14
     const area = (3.14 * parseFloat(a) * parseFloat(b)).toFixed(2);
     calculation(serial,shapeName,area);
 });
 
+
+// function for get input value by id
+function getInputValueById(inputId){
+    const shapeNameElement = document.getElementById(inputId).innerText;
+    return shapeNameElement;
+}
+
+
+// Function for calculation - Show the data
 function calculation(serial,shapeName,area){
     const tableContainer =  document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML = `
-        <td style="padding: 20px"> ${serial} </td>
-        <td style="padding: 20px"> ${shapeName} </td>
-        <td style="padding: 20px"> ${area}cm<sup>2</sup> </td>
+        <td>${serial}</td>
+        <td>${shapeName}</td>
+        <td>${area}cm<sup>2</sup></td>
+        <td><button style="background-color:#1090d8;padding:6px 12px;color: white;border-radius:4px;">convert to m<sup>2</sup></button></td>
     `    
     tableContainer.appendChild(tr);
-
 }
